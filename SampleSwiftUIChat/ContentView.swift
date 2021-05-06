69d120112041eb54f33decbcec54014a2da95ba4
yoonseok312 <yoonseok@berkeley.edu>
2021-05-06 10:56:20 +0900

    :sparkles: Sample SwiftUIChatApp
    Sample SwiftUIChatApp

---
+++ SampleSwiftUIChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleSwiftUIChat
+//
+//  Created by yoonseok312 on 2021/05/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleSwiftUIChat/SampleSwiftUIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSwiftUIChatApp.swift
+//  SampleSwiftUIChat
+//
+//  Created by yoonseok312 on 2021/05/06.
+//
+
+import SwiftUI
+
+@main
+struct SampleSwiftUIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
