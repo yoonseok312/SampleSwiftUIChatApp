25112580027718a105de4cfd09d3f85392394cb8
yoonseok312 <yoonseok@berkeley.edu>
2021-05-06 11:03:30 +0900

    :sparkles: SampleChat

---
+++ SampleChat/SampleChat/SampleChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleChatApp.swift
+//  SampleChat
+//
+//  Created by yoonseok312 on 2021/05/06.
+//
+
+import SwiftUI
+
+@main
+struct SampleChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
